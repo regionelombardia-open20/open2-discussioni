e7283a17a736e1dad949db06fcf6422e8a798674
Aria S.p.a <info@ariaspa.it>
2022-06-30 18:38:37 +0200

    Automated Release of version 1.7.0

--- src/views/email/notify_summary.php
+++ src/views/email/notify_summary.php
@@ -1,58 +1,60 @@
 
-<?php foreach ($arrayModels as $model) {
+<?php
+foreach ($arrayModels as $model) {
     $url = '/img/img_default.jpg';
     if (!is_null($model->discussionsTopicImage)) {
         $url = $model->discussionsTopicImage->getUrl('original', false, true);
-    }?>
-<tr>
-    <td colspan="2" style="padding-bottom:10px;">
-        <table cellspacing="0" cellpadding="0" border="0" align="center"   class="email-container" width="100%">
-            <tr>
-                <td bgcolor="#FFFFFF" style="padding:10px 15px 10px 15px;">
-                    <table width="100%">
-                        <tr>
-                            <td style="font-size:18px; font-weight:bold; padding: 5px 0 ; font-family: sans-serif;"><p style="margin:0">
-                                    <?= \yii\helpers\Html::a($model->getTitle(), Yii::$app->urlManager->createAbsoluteUrl($model->getFullViewUrl()), ['style' => 'color: #000;']) ?>
-                                </p>
-                                <p style="font-size:13px; color:#7d7d7d; padding:10px 0; font-family: sans-serif; font-weight:normal; margin:0"><?=  $model->getDescription(true); ?></p>
-                            </td>
-                            <td width="100" valign="top" align="right"><img src="<?= $url?>" width="85" height="85" border="0" align="center"></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2" style="padding:0;">
-                                <table width="100%">
-                                    <tr>
-                                        <td width="400">
-                                            <table width="100%">
-                                                <tr>
-                                                    <td style="box-sizing:border-box;padding-bottom: 5px;">
-                                                        <div style="margin-top:20px; display: flex; padding: 10px;">
-                                                            <?= \open20\amos\core\forms\ItemAndCardHeaderWidget::widget([
-                                                                'model' => $model,
-                                                                'publicationDateNotPresent' => true,
-                                                                'showPrevalentPartnershipAndTargets' => true,
-                                                                'absoluteUrlAvatar' => true,
-                                                            ]); ?>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            </table>
-                                        </td>
-                                        <td align="right" width="85" height="20" valign="bottom" style="text-align: center; padding-left: 10px;"><a href="<?= Yii::$app->urlManager->createAbsoluteUrl($model->getFullViewUrl())?>" style="background: #297A38; border:3px solid #297A38; color: #ffffff; font-family: sans-serif; font-size: 11px; line-height: 22px; text-align: center; text-decoration: none; display: block; font-weight: bold; text-transform: uppercase; height: 20px;" class="button-a">
-                                                <!--[if mso]>&nbsp;&nbsp;&nbsp;&nbsp;<![endif]-->Partecipa<!--[if mso]>&nbsp;&nbsp;&nbsp;&nbsp;<![endif]-->
-                                            </a></td>
-                                    </tr>
+    }
+    ?>
+    <tr>
+        <td colspan="2" style="padding-bottom:10px;">
+            <table cellspacing="0" cellpadding="0" border="0" align="center"   class="email-container" width="100%">
+                <tr>
+                    <td bgcolor="#FFFFFF" style="padding:10px 15px 10px 15px;">
+                        <table width="100%">
+                            <tr>
+                                <td style="font-size:18px; font-weight:bold; padding: 5px 0 ; font-family: sans-serif;"><p style="margin:0">
+                                        <?= \yii\helpers\Html::a($model->getTitle(),
+                                            Yii::$app->urlManager->createAbsoluteUrl($model->getFullViewUrl()),
+                                            ['style' => 'color: #000; text-decoration:none;'])
+                                        ?>
+                                    </p>
+                                    <p style="font-size:13px; color:#7d7d7d; padding:10px 0; font-family: sans-serif; font-weight:normal; margin:0"><?= $model->getDescription(true); ?></p>
+                                </td>
+                                <td width="100" valign="top" align="right"><img src="<?= $url ?>" width="85" height="85" border="0" align="center"></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2" style="padding:0;">
+                                    <table width="100%">
+                                        <tr>
+                                            <td width="400">
+                                                <table width="100%">
+                                                    <tr>
+                                                        <?=
+                                                        \open20\amos\notificationmanager\widgets\ItemAndCardWidgetEmailSummaryWidget::widget([
+                                                            'model' => $model,
+                                                        ]);
+                                                        ?>
 
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2" style="border-bottom:1px solid #D8D8D8; padding:5px 0px"></td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-        </table>
-    </td>
-</tr>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                            <td align="right" width="85" valign="bottom" style="text-align: center; padding-left: 10px;">
+                                                <a href="<?= Yii::$app->urlManager->createAbsoluteUrl($model->getFullViewUrl()) ?>" style="background: #297A38; border:3px solid #297A38; color: #ffffff; font-family: sans-serif; font-size: 11px; line-height: 22px; text-align: center; text-decoration: none; display: block; font-weight: bold; text-transform: uppercase; height: 20px;" class="button-a">
+                                                    <!--[if mso]>&nbsp;&nbsp;&nbsp;&nbsp;<![endif]-->Partecipa<!--[if mso]>&nbsp;&nbsp;&nbsp;&nbsp;<![endif]-->
+                                                </a></td>
+                                        </tr>
+
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2" style="border-bottom:1px solid #D8D8D8; padding:5px 0px"></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
 <?php } ?>
--- src/views/email/notify_summary_network.php
+++ src/views/email/notify_summary_network.php
@@ -1,28 +1,33 @@
 <?php
+
 use \open20\amos\notificationmanager\AmosNotify;
+
+$colors = \open20\amos\notificationmanager\utility\NotifyUtility::getColorNetwork($color);
 ?>
-<?php foreach ($arrayModels as $model) {
-    $url = '/img/img_default.jpg';
+<?php
+foreach ($arrayModels as $model) {
+    $textCallToAction = 'Partecipa';
+    $url              = '/img/img_default.jpg';
     if (!is_null($model->discussionsTopicImage)) {
         $url = $model->discussionsTopicImage->getUrl('original', false, true);
     }
 //
-            $count = 0;
-            if(!empty($arrayModelsComments)) {
+    $count = 0;
+    if (!empty($arrayModelsComments)) {
 //                $count = count($arrayModelsComments);
 //                echo "<h2>$count Commenti nuovi</h2>";
-                if(!empty($arrayModelsComments[$model->id])) {
-                    $count = count($arrayModelsComments[$model->id]);
+        if (!empty($arrayModelsComments[$model->id])) {
+            $count = count($arrayModelsComments[$model->id]);
 //                    foreach ($arrayModelsComments[$model->id] as $comment) {
 //                        echo $this->render('content_comment', ['comment' => $comment]);
 //                    }
-                }
-            }
-            if($count == 1){
-                $nuovoText = AmosNotify::t('amosnotify', 'nuovo commento');
-            } else {
-                $nuovoText = AmosNotify::t('amosnotify', 'nuovi commenti');
-            }
+        }
+    }
+    if ($count == 1) {
+        $nuovoText = AmosNotify::t('amosnotify', 'nuovo commento');
+    } else {
+        $nuovoText = AmosNotify::t('amosnotify', 'nuovi commenti');
+    }
     ?>
 
 
@@ -31,12 +36,21 @@ use \open20\amos\notificationmanager\AmosNotify;
             <table width="100%">
                 <tr>
                     <td valigh="top" style="font-size:16px; font-weight:bold; padding: 5px 15px 5px 0px; font-family: sans-serif; text-align:left; vertical-align:top;"><p style="margin:0 0 5px 0">
-                            <?= \yii\helpers\Html::a($model->getTitle(), Yii::$app->urlManager->createAbsoluteUrl($model->getFullViewUrl()), ['style' => 'color: #000;']) ?>
-                            <?php if($count > 0) { ?>
-                                <span style="display: inline-block; margin: 2px;text-transform: uppercase; color: #D44141; border: 1px solid #D44141; height: 16px; line-height: 16px; font-size: 10px; vertical-align:top; padding: 0 5px;" class="tag">
+                            <?=
+                            \yii\helpers\Html::a($model->getTitle(),
+                                Yii::$app->urlManager->createAbsoluteUrl($model->getFullViewUrl()),
+                                ['style' => 'color: #000; text-decoration:none;'])
+                            ?>
+                            <?php if ($count > 0) { ?>
+                                <span style="display: inline-block; margin: 2px;text-transform: uppercase; color: <?= $colors[1] ?>; border: 1px solid <?= $colors[1] ?>; height: 16px; line-height: 16px; font-size: 10px; vertical-align:top; padding: 0 5px;" class="tag">
                                     <?= $count ?> <?= $nuovoText ?>
                                 </span></p>
-                            <?php } ?>
+                        <?php } else { ?>
+
+                            <span style="display: inline-block; margin: 2px;text-transform: uppercase; color: <?= $colors[1] ?>; border: 1px solid <?= $colors[1] ?>; height: 16px; line-height: 16px; font-size: 10px; vertical-align:top; padding: 0 5px;" class="tag">
+                                <?= AmosNotify::t('amosnotify', 'nuova discussione') ?>
+                            </span></p>
+                        <?php } ?>
                     </td>
                 </tr>
                 <tr>
@@ -45,34 +59,45 @@ use \open20\amos\notificationmanager\AmosNotify;
                             <tr>
                                 <td width="400" style="text-align:left;">
                                     <table width="100%">
-                                        <tr>
-                                            <td style="box-sizing:border-box;padding-bottom: 5px;">
-                                                <div style="margin-top:20px; display: flex; padding: 10px;">
-                                                    <?= \open20\amos\core\forms\ItemAndCardHeaderWidget::widget([
-                                                        'model' => $model,
-                                                        'publicationDateNotPresent' => true,
-                                                        'showPrevalentPartnershipAndTargets' => true,
-                                                        'absoluteUrlAvatar' => true,
-                                                    ]); ?>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <?php if($count ==1){
-                                            $comment = $arrayModelsComments[$model->id][0];
-                                            $textComment = '';
-                                            if(!empty($comment)){
-                                                $textComment = $comment->comment_text;
-                                            }?>
+                                        <?php
+                                        if ($count == 1) {
+                                            $textCallToAction = 'Rispondi';
+                                            $comment          = $arrayModelsComments[$model->id][0];
+                                            $textComment      = '';
+                                            if (!empty($comment)) {
+                                                $textComment = strip_tags($comment->comment_text);
+                                                ?>
+
+                                                <tr>
+                                                    <td colspan="2" style="text-align:left;">
+                                                        <strong style="font-family: sans-serif; font-size:11px; color:#000"><?= $comment->createdUserProfile ?></strong>
+                                                    </td>
+                                                </tr>
+                                            <?php } ?>
                                             <tr>
-                                                <td>
+                                                <td colspan="2" style="text-align:left;">
                                                     <em style="font-family: sans-serif; font-size:14px; color:#000">"<?= $textComment ?>"</em>
                                                 </td>
                                             </tr>
-                                        <?php }?>
+                                            <?php
+                                        } else if ($count > 1) {
+                                            $textCallToAction = 'Leggi';
+                                            ?>
+                                        <?php } else { ?>
+                                            <tr>
+                                                <?=
+                                                \open20\amos\notificationmanager\widgets\ItemAndCardWidgetEmailSummaryWidget::widget([
+                                                    'model' => $model,
+                                                ]);
+                                                ?>
+                                            </tr>
+                                        <?php } ?>
                                     </table>
                                 </td>
-                                <td align="right" width="85" height="20" valign="bottom" style="text-align: center; padding-left: 10px;"><a href="<?=  Yii::$app->urlManager->createAbsoluteUrl($model->getFullViewUrl()) ?>" style="background: #D44141; border:3px solid #D44141; color: #ffffff; font-family: sans-serif; font-size: 11px; line-height: 22px; text-align: center; text-decoration: none; display: block; font-weight: bold; text-transform: uppercase; height: 20px;" class="button-a">
-                                        <!--[if mso]>&nbsp;&nbsp;&nbsp;&nbsp;<![endif]-->Rispondi<!--[if mso]>&nbsp;&nbsp;&nbsp;&nbsp;<![endif]-->
+                                <td align="right" width="85" height="20" valign="bottom" style="text-align: center; padding-left: 10px;"><a href="<?= Yii::$app->urlManager->createAbsoluteUrl($model->getFullViewUrl()) ?>" style="background: <?= $colors[1] ?>; border:3px solid <?= $colors[1] ?>; color: #ffffff; font-family: sans-serif; font-size: 11px; line-height: 22px; text-align: center; text-decoration: none; display: block; font-weight: bold; text-transform: uppercase; height: 20px;" class="button-a">
+                                        <!--[if mso]>&nbsp;&nbsp;&nbsp;&nbsp;<![endif]--><?=
+                                        \Yii::t('amosapp', $textCallToAction)
+                                        ?><!--[if mso]>&nbsp;&nbsp;&nbsp;&nbsp;<![endif]-->
                                     </a></td>
                             </tr>
 
@@ -83,7 +108,7 @@ use \open20\amos\notificationmanager\AmosNotify;
             </table>
         </td>
     </tr>
-<!--                --><?php
+    <!--                --><?php
 //            if(!empty($arrayModelsComments)) {
 //                $count = count($arrayModelsComments);
 //                echo "<h2>$count Commenti nuovi</h2>";
@@ -92,5 +117,6 @@ use \open20\amos\notificationmanager\AmosNotify;
 //                        echo $this->render('content_comment', ['comment' => $comment]);
 //                    }
 //                }
-//            }?>
+//            }
+    ?>
 <?php } ?>
